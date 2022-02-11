4d5ae8b238fb58c1db24abd98f657702a29ad707
jonathan-cederquist <97691358+jonathan-cederquist@users.noreply.github.com>
2022-02-10 17:19:07 -0800

    Very close

--- src/StepResponse.py
+++ src/StepResponse.py
@@ -15,51 +15,69 @@ import serial
 from matplotlib import pyplot as pp
 
 while True:
-    # initialize data arrays to plot
-    timeList = []
-    positionList = []
+    
+    try:
+        # initialize data arrays to plot
+        timeList = []
+        positionList = []
+        badVals = []
+    
+        # open serial communication
+        with serial.Serial ('COM3', 115200, timeout = 1) as s_port:
+            
+            # prompts user for Kp, and codes string into a byte
+            kp = bytes(input("Run step response? [Press 'Enter']"), 'utf-8')
+            # writes user specified Kp to serial communication and "enters"
+            s_port.write(b'\r')
+    
+            # motor choice functionality does not exist yet
+            # motor = bytes(input("Specify which motor to run [1,2]: "), 'utf-8')
+            # s_port.write(motor, b'\r')
+            
+            # need to read first line in advance because that's where the enter command input is
+            s_port.readline()
+            
+            while s_port.inWaiting==0:
+                pass
+            
+            print(s_port.inWaiting)
+            
+            # infinite loop 
+            while s_port.inWaiting != 0:
+                try:
+                    line = s_port.readline().strip(b'\r\n').split(b' ')
+                    val1 = line[0].decode()
+                    val2 = line[1].decode()
+                    
+                    timeList.append(float(val1))
+                    positionList.append(float(val2))
+                    
+                    # print to track progress and trace errors
+                    print(line)
+                    
+                # stops data collection when readline takes in the
+                # line that prompts for an Kp input
+                except ValueError:
+                    badVals.append(line)
+                    
+                except IndexError:
+                    break
+    
+        print("Data Collection Complete")
+    
+        # PLOTTING
+        fig, plt = pp.subplots()
+        plt.plot(timeList, positionList)
+        plt.set(xlabel = "Time [ms]", ylabel = "Voltage")
+        plt.set(title = "RC Circuit Step Response")
+    
+        # set y limits of plot in radians
+        plt.set_ylim(0,4)
+    
+        # display plot
+        pp.show()
+    
+    except KeyboardInterrupt:
+        break
 
-    # open serial communication
-    with serial.Serial ('COM3', 115200, timeout = 1) as s_port:
-        
-        # prompts user for Kp, and codes string into a byte
-        kp = bytes(input("Run step response? [Press 'Enter']"), 'utf-8')
-        # writes user specified Kp to serial communication and "enters"
-        s_port.write(b'\r')
-
-        # motor choice functionality does not exist yet
-        # motor = bytes(input("Specify which motor to run [1,2]: "), 'utf-8')
-        # s_port.write(motor, b'\r')
-        
-        # need to read first line in advance because that's where the kp input is
-        s_port.readline()
-        
-        
-        # infinite loop 
-        while True:
-            try:
-                line = s_port.readline().strip(b'\r\n').split(b' ')
-                timeList.append(float(line[0].decode()))
-                positionList.append(float(line[1].decode()))
-                
-                # print to track progress and trace errors
-                print(line)
-                
-            # stops data collection when readline takes in the
-            # line that prompts for an Kp input
-            except ValueError:
-                break
-
-    print("Data Collection Complete")
-
-    # PLOTTING
-    fig, plt = pp.subplots()
-    plt.plot(timeList, positionList)
-    plt.set(xlabel = "Time [ms]", ylabel = "Voltage")
-    plt.set(title = "RC Circuit Step Response")
-
-    # set y limits of plot in radians
-    #plt.set_ylim(0,7)
-
-    # display plot
-    pp.show()
+print('\nGoodbye')
--- src/main.py
+++ src/main.py
@@ -19,10 +19,11 @@ import micropython
 micropython.alloc_emergency_exception_buf(100)
 
 # ADC data read-in setup
-adc = pyb.ADC(pyb.Pin.board.PA5)
-ADC_data = task_share.Queue('h', 1000, thread_protect = False, name = "ADC_data")
+adc = pyb.ADC(pyb.Pin.board.PC0)
+ADC_data = task_share.Queue('h', 1100, thread_protect = False, name = "ADC_data")
 count = 0
-MAX_data = 100
+MAX_data = 1100
+#v_ref = adc.read_vref()
 
 # interrupt setup
 tim1 = pyb.Timer (1, freq=1000)
@@ -35,20 +36,24 @@ def read_ADC(which_timer):
         tim1.callback(None)
 
 # Step-response output setup
-out_PC1 = pyb.Pin (pyb.Pin.board.PA4, pyb.Pin.OUT_PP)
+out_PC1 = pyb.Pin (pyb.Pin.board.PC1, pyb.Pin.OUT_PP)
 
 if __name__ == "__main__":    
     
+    out_PC1.low()
+    
     while True:
         ADC_data.clear()
-        out_PC1.low()
         count = 0
+        out_PC1.low()
         input("Run step response? [Press 'Enter']")
         tim1.callback(read_ADC)
         print(0, ADC_data.get())
         out_PC1.high()
         start = utime.ticks_ms()
+        
         for i in range(1, MAX_data): # roughly 1000 milliseconds
             if ADC_data.any():
-                print(i, ADC_data.get())                
+                print(i, (ADC_data.get()/4095)*3.3)
+        print("Done")
         
